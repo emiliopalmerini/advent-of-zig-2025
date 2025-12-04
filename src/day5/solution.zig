ead862c551c4c7e12ccab4288a459f2b011b9d8c
emiliopalmerini <emilio.palmerini@gmail.com>
2025-12-04 06:56:11 +0100

    day05

---
+++ src/day5/solution.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+pub fn solvePart1() !void {}
+
+pub fn solvePart2() !void {}
+
+pub fn main() !void {
+    // const p1 = solvePart1();
+    // std.debug.print("Part 1: {d}\n", .{p1});
+    //
+    // const p2 = try solvePart2();
+    // std.debug.print("Part 2: {d}\n", .{p2});
+}
---
+++ tests/day5.zig
@@ -0,0 +1,2 @@
+const std = @import("std");
+const day5 = @import("advent_of_zig_2025").day5;

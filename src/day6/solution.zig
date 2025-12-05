d45afc5968af2f7e55406562f929674b54c6149b
emiliopalmerini <emilio.palmerini@gmail.com>
2025-12-05 07:24:23 +0100

    setup day06

---
+++ src/day6/solution.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const u = @import("utils");
+
+// pub fn solvePart1(allocator: std.mem.Allocator, input: []const u8) !u64 {
+// }
+
+// pub fn solvePart2(allocator: std.mem.Allocator, input: []const u8) !u64 {
+// }
+
+pub fn main() !void {
+    // const data = @embedFile("input.txt");
+
+    // const p1 = try solvePart1(allocator, data);
+    // std.debug.print("Part 1: {d}\n", .{p1});
+    //
+    // const p2 = try solvePart2(allocator, data);
+    // std.debug.print("Part 2: {d}\n", .{p2});
+}

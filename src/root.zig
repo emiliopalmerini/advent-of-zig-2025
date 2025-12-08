283f7f8b98edac90f45d454467b2cbc2bbab273d
emiliopalmerini <emilio.palmerini@gmail.com>
2025-12-08 01:02:52 +0100

    refactor: this change was ultimately unnecessary

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,8 @@
-pub const day1 = @import("day1/solution.zig");
-pub const day2 = @import("day2/solution.zig");
-pub const day3 = @import("day3/solution.zig");
-pub const day4 = @import("day4/solution.zig");
-pub const day5 = @import("day5/solution.zig");
-pub const day6 = @import("day6/solution.zig");
-pub const day7 = @import("day7/solution.zig");
-pub const day8 = @import("day8/solution.zig");
+pub const day1 = @import("day01/solution.zig");
+pub const day2 = @import("day02/solution.zig");
+pub const day3 = @import("day03/solution.zig");
+pub const day4 = @import("day04/solution.zig");
+pub const day5 = @import("day05/solution.zig");
+pub const day6 = @import("day06/solution.zig");
+pub const day7 = @import("day07/solution.zig");
+pub const day8 = @import("day08/solution.zig");
--- tests/root.zig
+++ tests/root.zig
@@ -1,12 +1,12 @@
 // Re-export test modules
-pub const day1_tests = @import("day1.zig");
-pub const day2_tests = @import("day2.zig");
-pub const day3_tests = @import("day3.zig");
-pub const day4_tests = @import("day4.zig");
-pub const day5_tests = @import("day5.zig");
-pub const day6_tests = @import("day6.zig");
-pub const day7_tests = @import("day7.zig");
-pub const day8_tests = @import("day8.zig");
+pub const day1_tests = @import("day01.zig");
+pub const day2_tests = @import("day02.zig");
+pub const day3_tests = @import("day03.zig");
+pub const day4_tests = @import("day04.zig");
+pub const day5_tests = @import("day05.zig");
+pub const day6_tests = @import("day06.zig");
+pub const day7_tests = @import("day07.zig");
+pub const day8_tests = @import("day08.zig");
 
 // Force compilation by using the modules
 comptime {
